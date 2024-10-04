de02adfaf098e01cc162e0e2e95048e5110c5483
Дарья <darya_sv03@mail.ru>
2024-10-04 13:36:44 +0300

    Fourth commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-
+<a href="https://codeclimate.com/github/ze4dori/labs/maintainability"><img src="https://api.codeclimate.com/v1/badges/617daa5d6acb80f85746/maintainability"/></a>
 
 <b>Демонстрация<b/> различных исходов игры: [Asciinema](https://asciinema.org/a/ToNVjeBXsCldSDizVtjwpg4jN).
 
--- game/src/games/nok.py
+++ game/src/games/nok.py
@@ -6,10 +6,10 @@ def lcm(a, b, c):
     return abs(ab_lcm * c) // math.gcd(ab_lcm, c)
 
 def generate_numbers():
-    num1 = random.randint(1, 100)
-    num2 = random.randint(1, 100)
-    num3 = random.randint(1, 100)
-    question = f"{num1} {num2} {num3}"
-    correct_answer = str(lcm(num1, num2, num3))
+    first_number = random.randint(1, 100)
+    second_number = random.randint(1, 100)
+    third_number = random.randint(1, 100)
+    question = f"{first_number} {second_number} {third_number}"
+    correct_answer = str(lcm(first_number, second_number, third_number))
 
     return question, correct_answer
